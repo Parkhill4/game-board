356ac38f24a289ba678384cc5131e342f6de8be3
allysongarcia <98438254+allysongarcia@users.noreply.github.com>
2022-11-28 04:13:59 -0600

    2

---
+++ Assets/gamePiece.cs
@@ -0,0 +1,27 @@
+/*using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class gamePiece : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    public diceRollScript value;
+    public move StartingTile;
+    move currentTile;
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    int spacesToMove = value.DiceValue;
+    
+    
+}*/
